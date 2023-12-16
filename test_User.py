931ed4d7eef65798c49784f5f2392c3d47e35130
Jevilka <154011910+Jevilka@users.noreply.github.com>
2023-12-16 22:44:33 +0200

    Update test_User.py

--- test_User.py
+++ test_User.py
@@ -3,11 +3,9 @@ from User import User
 
 class TestUserMethods(unittest.TestCase):
     def test_add_user(self):
-        # Ваш код для теста добавления пользователя
         pass
 
     def test_remove_user(self):
-        # Ваш код для теста удаления пользователя
         pass
 
 if __name__ == '__main__':
