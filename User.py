641da4a055725709fe289b8d38de10e228d111d4
Jevilka <154011910+Jevilka@users.noreply.github.com>
2023-12-16 22:39:27 +0200

    Add files via upload

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, user_id, name):
+        self.ID = user_id
+        self.Name = name
+
+def add_user(self, user):
+    # Ваш код для добавления пользователя
+
+def remove_user(self, user_id):
+    # Ваш код для удаления пользователя
---
+++ test_User.py
@@ -0,0 +1,14 @@
+import unittest
+from User import User
+
+class TestUserMethods(unittest.TestCase):
+    def test_add_user(self):
+        # Ваш код для теста добавления пользователя
+        pass
+
+    def test_remove_user(self):
+        # Ваш код для теста удаления пользователя
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
