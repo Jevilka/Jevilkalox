76e0807f0bc95dcc6a05a02c2b70f47661578d3c
Jevilka <154011910+Jevilka@users.noreply.github.com>
2023-12-16 23:08:59 +0200

    Add files via upload

---
+++ test_Userservice.py
@@ -0,0 +1,19 @@
+import unittest
+from UserService import UserService
+from User import User
+
+class TestUserServiceMethods(unittest.TestCase):
+    def test_read_user(self):
+        pass
+
+    def test_add_user(self):
+        pass
+
+    def test_remove_user(self):
+        pass
+
+    def test_update_user(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
